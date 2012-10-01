ee7ef5e76b670fcc0cc01776e9d0e8a8876032c4
John Korsnes <johnkors@gmail.com>
2012-10-01 15:42:59 +0200

    Added nuspec file for generating nuget packages

--- NoCommons.NET/Properties/AssemblyInfo.cs
+++ NoCommons.NET/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NoCommons.NET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The NoCommons library is a collection of helper classes for manipulation and validation of data specific to Norway and Norwegian citizens")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("John Korsnes")]
 [assembly: AssemblyProduct("NoCommons.NET")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © BEKK Open 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
