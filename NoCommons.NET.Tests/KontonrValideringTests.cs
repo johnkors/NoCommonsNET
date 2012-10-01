67eb5e16fabb9c5a925299375c54f6fa7f67c2ed
John Korsnes <johnkors@gmail.com>
2012-10-01 13:24:32 +0200

    Added unit test for spanish Iban

--- NoCommons.NET.Tests/KontonrValideringTests.cs
+++ NoCommons.NET.Tests/KontonrValideringTests.cs
@@ -153,6 +153,23 @@ namespace NoCommons.Tests
             Assert.IsTrue(KontonummerValidator.IsValid("97102749069"));
         }
 
+
+        /// http://www.ecbs.org/iban.htm
+        [TestMethod]
+        public void TestValidNorwegianIban()
+        {
+            var validIbanValue = "NO9386011117947";
+            Assert.IsTrue(IbanValidator.IsValid(validIbanValue));
+        }
+
+        [TestMethod]
+        public void TestValidSpanishIban()
+        {
+            var validSpanishIban = "ES9121000418450200051332";
+            Assert.IsTrue(IbanValidator.IsValid(validSpanishIban));
+        }
+
+
         private static void AssertMessageContains(ArgumentException argumentException, string errorSyntax)
         {
             bool containsText = argumentException.Message.Contains(errorSyntax);
--- NoCommons.NET/Banking/IbanValidator.cs
+++ NoCommons.NET/Banking/IbanValidator.cs
@@ -4,15 +4,14 @@ using System.Text;
 
 namespace NoCommons.Banking
 {
-    public static class IbanValidator
+    public class IbanValidator
     {
-        public static bool ValidateIBAN(string ibanValue)
+        public static bool IsValid(string ibanValue)
         {
             if (System.Text.RegularExpressions.Regex.IsMatch(ibanValue, "^[A-Z0-9]"))
             {
                 ibanValue = ibanValue.Replace(" ", string.Empty);
-                string iban =
-                ibanValue.Substring(4, ibanValue.Length - 4) + ibanValue.Substring(0, 4);
+                string iban = ibanValue.Substring(4, ibanValue.Length - 4) + ibanValue.Substring(0, 4);
                 const int asciiShift = 55;
                 var sb = new StringBuilder();
                 foreach (char c in iban)
