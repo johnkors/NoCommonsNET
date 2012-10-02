adfd9de003190458354ccdaf91ab1df90b639f88
John Korsnes <johnkors@gmail.com>
2012-10-02 12:15:59 +0200

    Some more IBAN tests. Refactored IBANValidator

--- NoCommons.Tests/IbanValidationTests.cs
+++ NoCommons.Tests/IbanValidationTests.cs
@@ -14,5 +14,18 @@ namespace NoCommons.Banking.Tests
         {
             Assert.IsTrue(IbanValidator.IsValid(ibanValue), string.Format("Invalid for {0}", country));
         }
+
+        [TestCase("NO93 8601 1117 947", "On valid print format, but not valid digital format")]
+        [TestCase("ES91 2100 0418 4502 0005 1332", "On valid print format, but not valid digital format")]
+        [TestCase("no9386011117947", "Lower case country code")]
+        [TestCase("9386011117947", "Missing country code")]
+        [TestCase("NO938601111794", "Missing end digit")]
+        [TestCase("", "Empty string")]
+        [TestCase(null, "null")]
+        public void InvalidIbansReturnsFalse(string ibanValue, string reason)
+        {
+            Assert.IsFalse(IbanValidator.IsValid(ibanValue), string.Format("Valid when {0}. ", reason));
+        }
+
     }
 }
--- NoCommons/Banking/IbanValidator.cs
+++ NoCommons/Banking/IbanValidator.cs
@@ -8,37 +8,59 @@ namespace NoCommons.Banking
     {
         public static bool IsValid(string ibanValue)
         {
+            try
+            {
+                return Validate(ibanValue);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        static bool Validate(string ibanValue)
+        {
             if (System.Text.RegularExpressions.Regex.IsMatch(ibanValue, "^[A-Z0-9]"))
             {
-                ibanValue = ibanValue.Replace(" ", string.Empty);
-                string iban = ibanValue.Substring(4, ibanValue.Length - 4) + ibanValue.Substring(0, 4);
-                const int asciiShift = 55;
-                var sb = new StringBuilder();
-                foreach (char c in iban)
-                {
-                    int v;
-                    if (Char.IsLetter(c))
-                    {
-                        v = c - asciiShift;
-                    }
-                    else
-                    {
-                        v = int.Parse(c.ToString(CultureInfo.InvariantCulture));
-                    }
-                    sb.Append(v);
-                }
-                string checkSumString = sb.ToString();
+                string ibanLeftShiftedBy4 = ibanValue.Substring(4, ibanValue.Length - 4) + ibanValue.Substring(0, 4);
+                var checkSumString = CheckSumString(ibanLeftShiftedBy4);
                 int checksum = int.Parse(checkSumString.Substring(0, 1));
-                for (int i = 1; i < checkSumString.Length; i++)
-                {
-                    int v = int.Parse(checkSumString.Substring(i, 1));
-                    checksum *= 10;
-                    checksum += v;
-                    checksum %= 97;
-                }
-                return checksum == 1;
+                return HasCorrectChecksum(checksum, checkSumString);
             }
             return false;
+        }
+
+        private static bool HasCorrectChecksum(int checksum, string checkSumString)
+        {
+            for (int i = 1; i < checkSumString.Length; i++)
+            {
+                int v = int.Parse(checkSumString.Substring(i, 1));
+                checksum *= 10;
+                checksum += v;
+                checksum %= 97;
+            }
+            return checksum == 1;
+        }
+
+        private static string CheckSumString(string ibanLeftShiftedBy4)
+        {
+            const int asciiShift = 55;
+            var sb = new StringBuilder();
+            foreach (char c in ibanLeftShiftedBy4)
+            {
+                int v;
+                if (Char.IsLetter(c))
+                {
+                    v = c - asciiShift;
+                }
+                else
+                {
+                    v = int.Parse(c.ToString(CultureInfo.InvariantCulture));
+                }
+                sb.Append(v);
+            }
+            string checkSumString = sb.ToString();
+            return checkSumString;
         }
     }
 }
