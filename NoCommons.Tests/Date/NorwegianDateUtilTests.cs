eb48b212f47af1bb73d0e6416644c11b1dc0b3cc
John Korsnes <johnkors@gmail.com>
2012-11-07 16:18:39 +0100

    NorwegianDateUtils with failing tests

---
+++ NoCommons.Tests/Date/NorwegianDateUtilTests.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using NoCommons.Date;
+
+namespace NoCommons.Tests.Date
+{
+    public class NorwegianDateUtilTest
+    {
+        [Test]
+        public void testAdd2DaysWithinSameWeek()
+        {
+            var date = new DateTime(21, 3, 2007);
+            NorwegianDateUtil.addWorkingDaysToDate(date, 2);
+
+            Assert.AreEqual(23, date.Day);
+        }
+
+        [Test]
+        public void testAdd2DaysToLastDayOfMonth()
+        {
+            var date = new DateTime(2007, 28, 2);
+            NorwegianDateUtil.addWorkingDaysToDate(date, 2);
+
+            Assert.AreEqual(2, date.Day);
+            Assert.AreEqual(3, date.Month);
+        }
+
+        [Test]
+        public void testAdd5DaysWithNoHolidays()
+        {
+            var date = new DateTime(2007, 03, 21);
+            NorwegianDateUtil.addWorkingDaysToDate(date, 5);
+
+            Assert.AreEqual(28, date.Day);
+        }
+
+        [Test]
+        public void testAdd5DaysBeforeEasterHoliday()
+        {
+            var date = new DateTime(2007, 4, 4);
+            NorwegianDateUtil.addWorkingDaysToDate(date, 5);
+
+            Assert.AreEqual(16, date.Day);
+        }
+
+        [Test]
+        public void testAdd5DaysBeforeNationalDay()
+        {
+            var date = new DateTime(2007, 5, 16);
+            NorwegianDateUtil.addWorkingDaysToDate(date, 5);
+
+            Assert.AreEqual(24, date.Day);
+        }
+
+        [Test]
+        public void testAdd5DaysBeforeChristmas()
+        {
+            var date = new DateTime(2007, 12, 21);
+            NorwegianDateUtil.addWorkingDaysToDate(date, 5);
+
+            Assert.AreEqual(2, date.Day);
+            Assert.AreEqual(1, date.Month);
+            Assert.AreEqual(2008, date.Year);
+        }
+
+        [Test]
+        public void testWorkingDays()
+        {
+            Assert.IsFalse(NorwegianDateUtil.isWorkingDay(new DateTime(2007,3,25)), "Sunday not working day");
+            Assert.IsTrue(NorwegianDateUtil.isWorkingDay(new DateTime(2007, 3, 26)), "Monday is working day");
+            Assert.IsFalse(NorwegianDateUtil.isWorkingDay(new DateTime(2007,1,1)), "New years day not working day");
+            Assert.IsFalse(NorwegianDateUtil.isWorkingDay(new DateTime(2007,4,8)), "Easter day not working day");
+        }
+
+        [Test]
+        public void testVariousNorwegianHolidays()
+        {
+            // Set dates
+            checkHoliday("01.01.2007");
+            checkHoliday("01.05.2007");
+            checkHoliday("17.05.2007");
+            checkHoliday("25.12.2007");
+            checkHoliday("26.12.2007");
+
+            // Movable dates 2007
+            checkHoliday("01.04.2007");
+            checkHoliday("05.04.2007");
+            checkHoliday("06.04.2007");
+            checkHoliday("08.04.2007");
+            checkHoliday("09.04.2007");
+            checkHoliday("17.05.2007");
+            checkHoliday("27.05.2007");
+            checkHoliday("28.05.2007");
+
+            // Movable dates 2008
+            checkHoliday("16.03.2008");
+            checkHoliday("20.03.2008");
+            checkHoliday("21.03.2008");
+            checkHoliday("23.03.2008");
+            checkHoliday("24.03.2008");
+            checkHoliday("01.05.2008");
+            checkHoliday("11.05.2008");
+            checkHoliday("12.05.2008");
+        }
+
+        [Test]
+        public void testGetAllNorwegianHolidaysForYear()
+        {
+            const string format = "dd.MM.yyyy";
+            var holidays = NorwegianDateUtil.getHolidays(2008);
+            Assert.AreEqual(12, holidays.Count);
+            Assert.AreEqual("01.01.2008", holidays[0].ToString(format));
+            Assert.AreEqual("16.03.2008", holidays[1].ToString(format));
+            Assert.AreEqual("20.03.2008", holidays[2].ToString(format));
+            Assert.AreEqual("21.03.2008", holidays[3].ToString(format));
+            Assert.AreEqual("23.03.2008", holidays[4].ToString(format));
+            Assert.AreEqual("24.03.2008", holidays[5].ToString(format));
+            Assert.AreEqual("01.05.2008", holidays[6].ToString(format));
+            Assert.AreEqual("11.05.2008", holidays[7].ToString(format));
+            Assert.AreEqual("12.05.2008", holidays[8].ToString(format));
+            Assert.AreEqual("17.05.2008", holidays[9].ToString(format));
+            Assert.AreEqual("25.12.2008", holidays[10].ToString(format));
+            Assert.AreEqual("26.12.2008", holidays[11].ToString(format));
+        }
+
+        private void checkHoliday(String date)
+        {
+            var dateTime = DateTime.ParseExact(date, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            Assert.AreEqual( dateTime  , NorwegianDateUtil.isHoliday(dateTime));
+        }
+    }
+}
---
+++ NoCommons/Date/NorwegianDateUtil.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NoCommons.Date
+{
+    public class NorwegianDateUtil
+    {
+        private static Dictionary<int, List<DateTime>> holidays;
+
+        /**
+         * Adds the given number of working days to the given date. A working day is
+         * specified as a regular Norwegian working day, excluding weekends and all
+         * national holidays.
+         * <p/>
+         * Example 1:<br/>
+         * - Add 5 working days to Wednesday 21.03.2007 -> Yields Wednesday
+         * 28.03.2007. (skipping saturday and sunday)<br/>
+         * <p/>
+         * Example 2:<br/>
+         * - Add 5 working days to Wednesday 04.04.2007 (day before
+         * easter-long-weekend) -> yields Monday 16.04.2007 (skipping 2 weekends and
+         * 3 weekday holidays).
+         *
+         * @param date
+         *            The original date.
+         * @param days
+         *            The number of working days to add.
+         * @return The new date.
+         */
+
+        public static DateTime addWorkingDaysToDate(DateTime date, int days)
+        {
+            var cal = date;
+            var workingDays = 0;
+            for (var i = 0; i < days; i++)
+            {
+                var newDate = date.AddDays(1);
+                while (!isWorkingDay(newDate))
+                {
+                    workingDays++;
+                }
+            }
+
+            return cal.AddDays(workingDays);
+        }
+
+        /**
+         * Will check if the given date is a working day. That is check if the given
+         * date is a weekend day or a national holiday.
+         *
+         * @param date
+         *            The date to check.
+         * @return true if the given date is a working day, false otherwise.
+         */
+
+        public static bool isWorkingDay(DateTime date)
+        {
+            return date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday
+                   && !isHoliday(date);
+        }
+
+        /**
+         * Check if given Date object is a holiday.
+         *
+         * @param date
+         *            The Date to check.
+         * @return true if holiday, false otherwise.
+         */
+
+        public static bool isHoliday(DateTime inDate)
+        {
+            var year = inDate.Year;
+            var yearSet = getHolidaySet(year);
+            foreach (DateTime aYearSet in yearSet)
+            {
+                var date = aYearSet;
+                if (checkDate(inDate, date))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * Return a sorted array of holidays for a given year.
+         *
+         * @param year
+         *            The year to get holidays for.
+         * @return The array of holidays, sorted by date.
+         */
+
+        public static List<DateTime> getHolidays(int year)
+        {
+            var days = getHolidaySet(year);
+            days.Sort();
+            return days;
+        }
+
+        /**
+         * Get a set of holidays for a given year.
+         *
+         * @param year
+         *            The year to get holidays for.
+         * @return The set of dates.
+         */
+
+        private static List<DateTime> getHolidaySet(int year)
+        {
+            if (holidays == null)
+            {
+                holidays = new Dictionary<int, List<DateTime>>();
+            }
+            if (!holidays.ContainsKey(year))
+            {
+                var yearSet = new List<DateTime>();
+
+                // Add set holidays.
+                yearSet.Add(getDate(1, 1, year));
+                yearSet.Add(getDate(1, 5, year));
+                yearSet.Add(getDate(17, 5, year));
+                yearSet.Add(getDate(25, 12, year));
+                yearSet.Add(getDate(26, 12, year));
+
+                // Add movable holidays - based on easter day.
+                var easterDay = getEasterDay(year);
+
+                // Sunday before easter.
+                yearSet.Add(rollGetDate(easterDay, -7));
+
+                // Thurday before easter.
+                yearSet.Add(rollGetDate(easterDay, -3));
+
+                // Friday before easter.
+                yearSet.Add(rollGetDate(easterDay, -2));
+
+                // Easter day.
+                yearSet.Add(easterDay);
+
+                // Second easter day.
+                yearSet.Add(rollGetDate(easterDay, 1));
+
+                // "Kristi himmelfart" day.
+                yearSet.Add(rollGetDate(easterDay, 39));
+
+                // "Pinse" day.
+                yearSet.Add(rollGetDate(easterDay, 49));
+
+                // Second "Pinse" day.
+                yearSet.Add(rollGetDate(easterDay, 50));
+
+                holidays.Add(year, yearSet);
+            }
+            return holidays[year];
+        }
+
+     
+
+ 
+
+        /**
+         * Calculates easter day (sunday) by using Spencer Jones formula found here:
+         * <a href="http://no.wikipedia.org/wiki/P%C3%A5skeformelen">Wikipedia -
+         * Påskeformelen</a>
+         *
+         * @param year
+         *            The year to calculate from.
+         * @return The Calendar object representing easter day for the given year.
+         */
+
+        private static DateTime getEasterDay(int year)
+        {
+            int a = year%19;
+            int b = year/100;
+            int c = year%100;
+            int d = b/4;
+            int e = b%4;
+            int f = (b + 8)/25;
+            int g = (b - f + 1)/3;
+            int h = ((19*a) + b - d - g + 15)%30;
+            int i = c/4;
+            int k = c%4;
+            int l = (32 + (2*e) + (2*i) - h - k)%7;
+            int m = (a + (11*h) + (22*l))/451;
+            int n = (h + l - (7*m) + 114)/31; // This is the month number.
+            int p = (h + l - (7*m) + 114)%31; // This is the date minus one.
+
+            return new DateTime(year, n - 1, p + 1);
+        }
+
+        /**
+         * Check if the given dates match on day and month.
+         *
+         * @param cal
+         *            The Calendar representing the first date.
+         * @param other
+         *            The Calendar representing the second date.
+         * @return true if they match, false otherwise.
+         */
+
+        private static bool checkDate(DateTime cal, DateTime other)
+        {
+            return checkDate(cal, other.Day, other.Month);
+        }
+
+        /**
+         * Check if the given date represents the given date and month.
+         *
+         * @param cal
+         *            The Calendar object representing date to check.
+         * @param date
+         *            The date.
+         * @param month
+         *            The month.
+         * @return true if they match, false otherwise.
+         */
+
+        private static bool checkDate(DateTime cal, int date, int month)
+        {
+            return cal.Day == date && cal.Month == month;
+        }
+
+
+        /**
+         * Add the given number of days to the calendar and convert to Date.
+         *
+         * @param calendar
+         *            The calendar to add to.
+         * @param days
+         *            The number of days to add.
+         * @return The date object given by the modified calendar.
+         */
+
+        private static DateTime rollGetDate(DateTime calendar, int days)
+        {
+            var added = calendar.AddDays(days);
+            return added;
+        }
+
+        /**
+         * Get the date for the given values.
+         *
+         * @param day
+         *            The day.
+         * @param month
+         *            The month.
+         * @param year
+         *            The year.
+         * @return The date represented by the given values.
+         */
+
+        private static DateTime getDate(int day, int month, int year)
+        {
+            //Calendar cal = Calendar.getInstance(TimeZone. GetTimeZone("Europe/Berlin"), new Locale("no", "NO"));
+
+            return new DateTime(year, month, day);
+        }
+    }
+}
