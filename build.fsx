4ef5b4f231ed02aab0960f780ff042a0b962f757
John10 Korsnes <johnkors@gmail.com>
2016-04-01 13:36:57 +0200

    Nuget-fixes. AssemblyInfoUpdate instead of replace

--- build.fsx
+++ build.fsx
@@ -1,12 +1,14 @@
 #r @"tools/packages/FAKE/tools/FakeLib.dll"
 open Fake
 open Fake.AssemblyInfoFile
+open Fake.Testing.NUnit3
 
 let assemblyVersion = getBuildParamOrDefault "version" "0.1"
 let nugetVersion = assemblyVersion + "-alfa0002"
 let packageOutputDir = "./.output"
 let nugetApiKey = environVarOrDefault "NugetOrgApiKey" "ENV-VARIABLE NugetOrgApiKey is missing"
 let project = "./NoCommons/NoCommons.csproj"
+let sln = "./NoCommons.NET.sln"
 let buildOutput = "./.build"
 
 Target "Clean" (fun() ->
@@ -14,13 +16,25 @@ Target "Clean" (fun() ->
 )
 
 Target "UpdateAssemblyInfo"(fun _ -> 
-    CreateCSharpAssemblyInfo "./NoCommons/Properties/AssemblyInfo.cs"
-        [ Attribute.Version assemblyVersion
-          Attribute.FileVersion assemblyVersion]
+    ReplaceAssemblyInfoVersions (fun p -> 
+        {
+            p with 
+                OutputFileName = "./NoCommons/Properties/AssemblyInfo.cs"
+                AssemblyFileVersion = assemblyVersion
+                AssemblyVersion = assemblyVersion
+        }) 
 )
 
 Target "Build" (fun _ ->
-    MSBuildRelease buildOutput "Rebuild" [project] |> ignore 
+    MSBuildRelease buildOutput "Rebuild" [sln] |> ignore 
+)
+
+Target "Test" (fun _ ->
+    !! ".build/*.Tests.dll"
+      |> NUnit3 (fun p ->
+          {  p with
+                WorkingDir = "./"                                             
+          })     
 )
 
 Target "CreatePackage" (fun _ ->
@@ -44,6 +58,7 @@ Target "Nothing" (fun x -> trace "Try a defined target")
 "Clean"
     ==> "UpdateAssemblyInfo"
     ==> "Build"
+    ==> "Test"
     ==> "CreatePackage"
 
 
