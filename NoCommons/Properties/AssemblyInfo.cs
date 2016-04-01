966559d1149c86949308eff1a31eb096797af832
John10 Korsnes <johnkors@gmail.com>
2016-04-01 11:45:38 +0200

    Added nuget pack step

--- NoCommons/Properties/AssemblyInfo.cs
+++ NoCommons/Properties/AssemblyInfo.cs
@@ -7,33 +7,13 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("NoCommons.NET")]
 [assembly: AssemblyDescription("The NoCommons library is a collection of helper classes for manipulation and validation of data specific to Norway and Norwegian citizens")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("John Korsnes")]
 [assembly: AssemblyProduct("NoCommons.NET")]
-[assembly: AssemblyCopyright("Copyright © BEKK Open 2012")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e6af897f-8939-4b35-a31b-86ae195ecb44")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+[assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("NoCommons.Tests")] 
 [assembly: AssemblyVersion("0.0.4.0")]
 [assembly: AssemblyFileVersion("0.0.4.0")]
-
--- build.fsx
+++ build.fsx
@@ -1,5 +1,40 @@
 #r @"tools/packages/FAKE/tools/FakeLib.dll"
-
 open Fake
+open Fake.AssemblyInfoFile
 
-trace ("Hello build!")
+let assemblyVersion = getBuildParamOrDefault "version" "0.3"
+let nugetVersion = assemblyVersion + "-alfa0001"
+let packageOutputDir = "./output"
+let nugetApiKey = environVarOrDefault "nuget-api-key" "ENV-VARIABLE nuget-api-key is missing"
+
+Target "Clean" (fun() ->
+    CleanDirs [packageOutputDir]
+)
+
+Target "UpdateAssemblyInfo"(fun _ -> 
+    trace "TODO"
+)
+
+Target "CreatePackage" (fun _ ->
+    
+    NuGet (fun p -> 
+        {p with
+            WorkingDir = "./tools/nuget/" 
+            Authors = ["John Korsnes"]
+            Project = "NoCommons.NET"
+            Description = "stuff"
+            OutputPath = packageOutputDir
+            Version = nugetVersion
+            AccessKey = "My-API-Key-In-NuGet"
+            Publish = false }) 
+        "./NoCommons/NoCommons.csproj"
+)
+
+Target "Nothing" (fun x -> trace "Try a defined target")
+
+"Clean"
+    ==> "UpdateAssemblyInfo"
+    ==> "CreatePackage"
+
+
+RunTargetOrDefault "CreatePackage"
