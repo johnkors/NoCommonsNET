9c26851f610d692f06b023b3ca2d7c60b20508f2
John10 Korsnes <johnkors@gmail.com>
2016-04-01 14:26:40 +0200

    Working from clean

--- build.fsx
+++ build.fsx
@@ -4,19 +4,20 @@ open Fake.AssemblyInfoFile
 open Fake.Testing.NUnit3
 
 let assemblyVersion = getBuildParamOrDefault "version" "0.1"
-let nugetVersion = assemblyVersion + "-alfa0002"
+let nugetVersion = assemblyVersion + "-alfa0003"
+let packageInputDir = "./.packagefiles/"
 let packageOutputDir = "./.output"
+
 let nugetApiKey = environVarOrDefault "NugetOrgApiKey" "ENV-VARIABLE NugetOrgApiKey is missing"
 let project = "./NoCommons/NoCommons.csproj"
+let nuspec = "./NoCommons/NoCommons.nuspec"
 let sln = "./NoCommons.NET.sln"
-let buildOutput = "./.build"
 
-
+let buildOutputTest = "./.testbuild"
 
 Target "Clean" (fun() ->
-    CleanDirs [buildOutput; packageOutputDir]
+    CleanDirs [buildOutputTest; packageInputDir; packageOutputDir]
 )
-
 
 Target "UpdateAssemblyInfo"(fun _ -> 
     ReplaceAssemblyInfoVersions (fun p -> 
@@ -32,32 +33,36 @@ Target "NugetRestore" (fun _ ->
     RestorePackages() 
 )
 
-
-Target "Build" (fun _ ->
-    MSBuildRelease buildOutput "Rebuild" [sln] |> ignore 
+Target "BuildTest" (fun _ ->
+    MSBuildRelease buildOutputTest "Clean;Rebuild" [sln] |> ignore 
 )
 
 Target "Test" (fun _ ->
-    !! ".build/*.Tests.dll"
+    !! ".testbuild/*.Tests.dll"
       |> NUnit3 (fun p ->
           {  p with
                 WorkingDir = "./"                                             
           })     
 )
 
-Target "CreatePackage" (fun _ ->
-    MSBuildDebug "" "Rebuild" [sln] |> ignore    
+Target "Buildpackage" (fun _ -> 
+    MSBuildDebug "" "Clean;Rebuild" [project] |> ignore    
+)
+
+Target "CreatePackage" (fun _ ->        
     NuGet (fun p -> 
         {p with            
-            WorkingDir = "./tools/nuget"
+            WorkingDir = "./"
             Authors = ["John Korsnes"]
-            Project = "NoCommons"            
+            Project = "NoCommons"
+            Title = "NoCommons.NET"                                    
             OutputPath = packageOutputDir
             Version = nugetVersion
             AccessKey = nugetApiKey
             Publish = false    
-            }) 
+            })
         project
+        
 )
 
 Target "Nothing" (fun x -> trace "Try a defined target")
@@ -65,8 +70,9 @@ Target "Nothing" (fun x -> trace "Try a defined target")
 "Clean"
     ==> "UpdateAssemblyInfo"
     ==> "NugetRestore"
-    ==> "Build"
+    ==> "BuildTest"
     ==> "Test"
+    ==> "Buildpackage"
     ==> "CreatePackage"
 
 
