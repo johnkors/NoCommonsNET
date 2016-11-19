4b668ba00ac209c04d90f84d17b1f02ba52512cd
John10 Korsnes <johnkors@gmail.com>
2016-11-19 11:43:40 +0100

    Add sample console apps targeting different platforms/frameworks we want to support

---
+++ ConsoleDotNet46/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleDotNet46
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hi, this is a fodselsnr: " + "12312312");
+			Console.ReadKey();
+		}
+	}
+}
---
+++ ConsoleDotNetCore1.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleDotNetCore1._1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			Console.WriteLine("Hi, this is a fodselsnr: " + "12312312");
+			Console.ReadKey();
+		}
+    }
+}
