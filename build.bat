70de190706e051ba1a4e2cd130cb57db01642214
john4ice <john.korsnes@ice.net>
2017-04-18 13:25:45 +0200

    Fix build script

--- build.bat
+++ build.bat
@@ -1,11 +1,11 @@
 powershell .\InstallDotNetCore.ps1
 
-.\.dotnetcli\dotnet restore NoCommons
-.\.dotnetcli\dotnet restore NoCommons.Tests
+.\.dotnetcli\dotnet restore NoCommons\NoCommons.csproj
+.\.dotnetcli\dotnet restore NoCommons.Tests\NoCommons.Tests.csproj
 
-.\.dotnetcli\dotnet build NoCommons
-.\.dotnetcli\dotnet build NoCommons.Tests
+.\.dotnetcli\dotnet build NoCommons\NoCommons.csproj
+.\.dotnetcli\dotnet build NoCommons.Tests\NoCommons.Tests.csproj
 
 .\.dotnetcli\dotnet test NoCommons.Tests\NoCommons.Tests.csproj
 
-.\.dotnetcli\dotnet pack NoCommons -c Release -o build 
+.\.dotnetcli\dotnet pack NoCommons\NoCommons.csproj -c Release -o build 
